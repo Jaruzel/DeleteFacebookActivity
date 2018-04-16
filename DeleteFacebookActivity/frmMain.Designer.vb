075c1b079549e614d440189d6931f90df134c1db
Jaruzel <Jaruzel@users.noreply.github.com>
2018-04-16 17:03:26 +0100

    Initial Upload of project

---
+++ DeleteFacebookActivity/frmMain.Designer.vb
@@ -0,0 +1,243 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+  Inherits System.Windows.Forms.Form
+
+  'Form overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+    Try
+      If disposing AndAlso components IsNot Nothing Then
+        components.Dispose()
+      End If
+    Finally
+      MyBase.Dispose(disposing)
+    End Try
+  End Sub
+
+  'Required by the Windows Form Designer
+  Private components As System.ComponentModel.IContainer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()> _
+  Private Sub InitializeComponent()
+    Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+    Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+    Me.TextBox1 = New System.Windows.Forms.TextBox()
+    Me.Button1 = New System.Windows.Forms.Button()
+    Me.Button2 = New System.Windows.Forms.Button()
+    Me.TextBox2 = New System.Windows.Forms.TextBox()
+    Me.Label1 = New System.Windows.Forms.Label()
+    Me.Label2 = New System.Windows.Forms.Label()
+    Me.Label3 = New System.Windows.Forms.Label()
+    Me.btnStop = New System.Windows.Forms.Button()
+    Me.Button3 = New System.Windows.Forms.Button()
+    Me.Button4 = New System.Windows.Forms.Button()
+    Me.Button5 = New System.Windows.Forms.Button()
+    Me.Button6 = New System.Windows.Forms.Button()
+    Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+    Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+    CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+    Me.SuspendLayout()
+    '
+    'WebBrowser1
+    '
+    Me.WebBrowser1.Location = New System.Drawing.Point(12, 145)
+    Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+    Me.WebBrowser1.Name = "WebBrowser1"
+    Me.WebBrowser1.Size = New System.Drawing.Size(581, 590)
+    Me.WebBrowser1.TabIndex = 0
+    '
+    'TextBox1
+    '
+    Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+    Me.TextBox1.Location = New System.Drawing.Point(602, 35)
+    Me.TextBox1.Multiline = True
+    Me.TextBox1.Name = "TextBox1"
+    Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+    Me.TextBox1.Size = New System.Drawing.Size(570, 700)
+    Me.TextBox1.TabIndex = 1
+    Me.TextBox1.WordWrap = False
+    '
+    'Button1
+    '
+    Me.Button1.Location = New System.Drawing.Point(15, 47)
+    Me.Button1.Name = "Button1"
+    Me.Button1.Size = New System.Drawing.Size(75, 23)
+    Me.Button1.TabIndex = 2
+    Me.Button1.Text = "Comments"
+    Me.Button1.UseVisualStyleBackColor = True
+    '
+    'Button2
+    '
+    Me.Button2.Location = New System.Drawing.Point(96, 47)
+    Me.Button2.Name = "Button2"
+    Me.Button2.Size = New System.Drawing.Size(75, 23)
+    Me.Button2.TabIndex = 3
+    Me.Button2.Text = "Likes"
+    Me.Button2.UseVisualStyleBackColor = True
+    '
+    'TextBox2
+    '
+    Me.TextBox2.BackColor = System.Drawing.Color.White
+    Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None
+    Me.TextBox2.ForeColor = System.Drawing.SystemColors.InfoText
+    Me.TextBox2.Location = New System.Drawing.Point(0, 741)
+    Me.TextBox2.Multiline = True
+    Me.TextBox2.Name = "TextBox2"
+    Me.TextBox2.ReadOnly = True
+    Me.TextBox2.Size = New System.Drawing.Size(1183, 23)
+    Me.TextBox2.TabIndex = 0
+    '
+    'Label1
+    '
+    Me.Label1.BackColor = System.Drawing.SystemColors.AppWorkspace
+    Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+    Me.Label1.ForeColor = System.Drawing.Color.White
+    Me.Label1.Location = New System.Drawing.Point(12, 9)
+    Me.Label1.Name = "Label1"
+    Me.Label1.Size = New System.Drawing.Size(581, 23)
+    Me.Label1.TabIndex = 4
+    Me.Label1.Text = "Select Activity to Start Erasing"
+    Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+    '
+    'Label2
+    '
+    Me.Label2.BackColor = System.Drawing.SystemColors.AppWorkspace
+    Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+    Me.Label2.ForeColor = System.Drawing.Color.White
+    Me.Label2.Location = New System.Drawing.Point(12, 119)
+    Me.Label2.Name = "Label2"
+    Me.Label2.Size = New System.Drawing.Size(581, 23)
+    Me.Label2.TabIndex = 5
+    Me.Label2.Text = "Web View"
+    Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+    '
+    'Label3
+    '
+    Me.Label3.BackColor = System.Drawing.SystemColors.AppWorkspace
+    Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+    Me.Label3.ForeColor = System.Drawing.Color.White
+    Me.Label3.Location = New System.Drawing.Point(602, 9)
+    Me.Label3.Name = "Label3"
+    Me.Label3.Size = New System.Drawing.Size(570, 23)
+    Me.Label3.TabIndex = 6
+    Me.Label3.Text = "Log Output"
+    Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+    '
+    'btnStop
+    '
+    Me.btnStop.Location = New System.Drawing.Point(518, 82)
+    Me.btnStop.Name = "btnStop"
+    Me.btnStop.Size = New System.Drawing.Size(75, 23)
+    Me.btnStop.TabIndex = 7
+    Me.btnStop.Text = "STOP"
+    Me.btnStop.UseVisualStyleBackColor = True
+    '
+    'Button3
+    '
+    Me.Button3.Location = New System.Drawing.Point(177, 47)
+    Me.Button3.Name = "Button3"
+    Me.Button3.Size = New System.Drawing.Size(75, 23)
+    Me.Button3.TabIndex = 8
+    Me.Button3.Text = "Posts"
+    Me.Button3.UseVisualStyleBackColor = True
+    '
+    'Button4
+    '
+    Me.Button4.Location = New System.Drawing.Point(258, 47)
+    Me.Button4.Name = "Button4"
+    Me.Button4.Size = New System.Drawing.Size(75, 23)
+    Me.Button4.TabIndex = 9
+    Me.Button4.Text = "Friends"
+    Me.Button4.UseVisualStyleBackColor = True
+    '
+    'Button5
+    '
+    Me.Button5.Location = New System.Drawing.Point(339, 47)
+    Me.Button5.Name = "Button5"
+    Me.Button5.Size = New System.Drawing.Size(75, 23)
+    Me.Button5.TabIndex = 10
+    Me.Button5.Text = "Life Events"
+    Me.Button5.UseVisualStyleBackColor = True
+    '
+    'Button6
+    '
+    Me.Button6.Location = New System.Drawing.Point(420, 47)
+    Me.Button6.Name = "Button6"
+    Me.Button6.Size = New System.Drawing.Size(75, 23)
+    Me.Button6.TabIndex = 11
+    Me.Button6.Text = "OTHER"
+    Me.Button6.UseVisualStyleBackColor = True
+    '
+    'CheckBox1
+    '
+    Me.CheckBox1.AutoSize = True
+    Me.CheckBox1.Location = New System.Drawing.Point(15, 86)
+    Me.CheckBox1.Name = "CheckBox1"
+    Me.CheckBox1.Size = New System.Drawing.Size(431, 17)
+    Me.CheckBox1.TabIndex = 12
+    Me.CheckBox1.Text = "Keep going until nothing is left to erase under the selected activity, or I click" &
+    " on STOP."
+    Me.CheckBox1.UseVisualStyleBackColor = True
+    '
+    'PictureBox1
+    '
+    Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+    Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+    Me.PictureBox1.Image = Global.DeleteFacebookActivity.My.Resources.Resources.happy
+    Me.PictureBox1.Location = New System.Drawing.Point(543, 44)
+    Me.PictureBox1.Name = "PictureBox1"
+    Me.PictureBox1.Size = New System.Drawing.Size(32, 32)
+    Me.PictureBox1.TabIndex = 13
+    Me.PictureBox1.TabStop = False
+    '
+    'Form1
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.ClientSize = New System.Drawing.Size(1184, 765)
+    Me.Controls.Add(Me.PictureBox1)
+    Me.Controls.Add(Me.CheckBox1)
+    Me.Controls.Add(Me.Button6)
+    Me.Controls.Add(Me.Button5)
+    Me.Controls.Add(Me.Button4)
+    Me.Controls.Add(Me.Button3)
+    Me.Controls.Add(Me.btnStop)
+    Me.Controls.Add(Me.Label3)
+    Me.Controls.Add(Me.Label2)
+    Me.Controls.Add(Me.Label1)
+    Me.Controls.Add(Me.Button2)
+    Me.Controls.Add(Me.TextBox2)
+    Me.Controls.Add(Me.Button1)
+    Me.Controls.Add(Me.TextBox1)
+    Me.Controls.Add(Me.WebBrowser1)
+    Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+    Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+    Me.MaximizeBox = False
+    Me.Name = "Form1"
+    Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+    Me.Text = "Facebook Activity Erase Tool"
+    CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+    Me.ResumeLayout(False)
+    Me.PerformLayout()
+
+  End Sub
+
+  Friend WithEvents WebBrowser1 As WebBrowser
+  Friend WithEvents TextBox1 As TextBox
+  Friend WithEvents Button1 As Button
+  Friend WithEvents Button2 As Button
+  Friend WithEvents TextBox2 As TextBox
+  Friend WithEvents Label1 As Label
+  Friend WithEvents Label2 As Label
+  Friend WithEvents Label3 As Label
+  Friend WithEvents btnStop As Button
+  Friend WithEvents Button3 As Button
+  Friend WithEvents Button4 As Button
+  Friend WithEvents Button5 As Button
+  Friend WithEvents Button6 As Button
+  Friend WithEvents CheckBox1 As CheckBox
+  Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ DeleteFacebookActivity/frmMain.vb
@@ -0,0 +1,341 @@
+﻿Imports System.Threading
+Public Class frmMain
+  Dim strActivityURL As String = ""
+  Dim blnIsBrowsing As Boolean = False
+  Dim blnFoundLinks As Boolean = False
+  Dim strURLList As String = ""
+  Dim strDeleteURLs As String = ""
+  Dim blnFoundDeeper As Boolean = False
+  Dim intLoadMoreLevels As Integer = 0
+  Dim blnStop As Boolean = True
+  Dim txtAppTitle As String = "Facebook Activity Erase Tool"
+  Dim strFacebookUserID As String = ""
+
+  Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Me.Show()
+    Application.DoEvents()
+    With Me.TextBox2
+      .Height = 14
+      .Left = 0
+      .Top = Me.ClientSize.Height - .Height
+      .Width = Me.ClientSize.Width
+      .BackColor = Color.LightPink
+    End With
+    ToggleButtons(True)
+    ShowWarning()
+
+    BrowseTo("https://mbasic.facebook.com/")
+    If Me.WebBrowser1.DocumentTitle.ToString.ToLower.Contains("log in") Then
+      Log("Please Login to Facebook via the 'Web View'.")
+      Log("Then:")
+      Log("1. Click on the 'Profile' button via the 'Web View'")
+      Log("2. Click on the 'Activity Log' link via the 'Web View'")
+      Log("3. Click on the button of the activity content you want to Erase.")
+      Log("")
+    Else
+      Log("Please Wait a moment...")
+      NavToActivityPage()
+      Log("")
+      Log("Ok! Now click on the button of the activity content you want to Erase.")
+    End If
+
+
+  End Sub
+  Sub ShowWarning()
+    Dim strText As String = ""
+    strText = strText & "WARNING!\n\n"
+    strText = strText & "This tool will DELETE all activities you have performed on Facebook.\n\n"
+    strText = strText & "You will NOT be able to restore them once they are gone!\n\n"
+    strText = strText & "You run this tool AT YOUR OWN RISK.\n\n"
+    strText = strText & "Use of this tool MAY result in Facebook banning your account.\n\n"
+    strText = strText & "I [Matt Owen/Jaruzel] am NOT LIABLE for any and all issues encountered due to the use of this tool against your Facebook account, or against other persons Facebook account.\n\n"
+    strText = strText & "Click on the OK button to proceed, ONLY if you AGREE TO and UNDERSTAND the above.\n\n"
+    strText = strText.Replace("\n", vbCrLf)
+
+    Dim dlgResult As DialogResult = MsgBox(strText, MsgBoxStyle.OkCancel + MsgBoxStyle.Exclamation, txtAppTitle)
+    If dlgResult = DialogResult.Cancel Then End
+
+  End Sub
+  Sub ToggleButtons(blnToggle As Boolean)
+    Me.Button1.Enabled = blnToggle
+    Me.Button2.Enabled = blnToggle
+    Me.Button3.Enabled = blnToggle
+    Me.Button4.Enabled = blnToggle
+    Me.Button5.Enabled = blnToggle
+    Me.Button6.Enabled = blnToggle
+
+    Me.CheckBox1.Enabled = blnToggle
+
+    If blnToggle = True Then btnStop.Enabled = False Else btnStop.Enabled = True
+
+  End Sub
+  Sub LetsGo(strID As String)
+    If strFacebookUserID = "" Then
+      MsgBox("No Facebook User ID detected, please manually navigate to your Activity Log page in the 'Web View' first.")
+      Exit Sub
+    End If
+
+    Dim dlgRes As DialogResult = MsgBox("About to Erase Facebook Activity." & vbCrLf & vbCrLf & "Do not click on anything in the 'Web View' whilst the tool is running!", MsgBoxStyle.Information + MsgBoxStyle.OkCancel, txtAppTitle)
+    If dlgRes = DialogResult.Cancel Then Exit Sub
+
+    Me.PictureBox1.Image = My.Resources.cry
+    Me.Icon = My.Resources.ICON2
+    Me.TextBox2.BackColor = Color.LightGreen
+    ToggleButtons(False)
+    StartErase(strID)
+    ToggleButtons(True)
+    Me.PictureBox1.Image = My.Resources.happy
+    Me.Icon = My.Resources.ICON
+    Me.TextBox2.BackColor = Color.LightPink
+    Me.TextBox1.Focus()
+
+  End Sub
+  Sub StartErase(strActivityID As String)
+    Dim blnDoDelete As Boolean = False
+    blnStop = False
+    Dim strFilter As String = "?log_filter=" & strActivityID
+    Do
+      blnDoDelete = False
+      strURLList = ""
+      strDeleteURLs = ""
+
+      BrowseTo(strActivityURL & strFilter)
+      Log("Top Level Scan...")
+      CollectURLs()
+
+      Dim strList As String = strURLList
+
+      Log("Month Level Scan...")
+      Dim arrURLs() As String = strList.Split(vbTab)
+      For Each uURL As String In arrURLs
+        BrowseTo(uURL)
+        CollectURLs()
+        If blnStop = True Then Exit Sub
+      Next
+
+      strList = strURLList
+      Log("Looking for 'Load More'...")
+      arrURLs = strList.Split(vbTab)
+      For Each uURL As String In arrURLs
+        intLoadMoreLevels = 1
+        LookForLM(uURL)
+        If blnStop = True Then Exit Sub
+      Next
+
+      strList = strURLList
+      Log("Delete Items Scan...")
+      arrURLs = strList.Split(vbTab)
+      For Each uURL As String In arrURLs
+        BrowseTo(uURL)
+        DeleteActivity()
+        If blnStop = True Then Exit Sub
+      Next
+
+      arrURLs = strDeleteURLs.Split(vbTab)
+      For Each uURL As String In arrURLs
+        If uURL.Trim <> "" Then
+          Thread.Sleep(500)
+          Log("Deleting..." & uURL)
+          BrowseTo(uURL)
+          blnDoDelete = True
+          If blnStop = True Then Exit Sub
+        End If
+      Next
+      If Me.CheckBox1.Checked = False Then Exit Do
+      Log("Starting over to ensure all items deleted...")
+    Loop Until blnDoDelete = False
+
+    Log("All Done - nothing left to delete (Hopefully!)")
+    WebBrowser1.Navigate(strActivityURL & strFilter)
+
+  End Sub
+  Sub BrowseTo(strURL As String)
+    blnIsBrowsing = True
+    WebBrowser1.Navigate(strURL)
+
+    Do
+      Application.DoEvents()
+    Loop Until blnIsBrowsing = False
+
+  End Sub
+  Sub DeleteActivity()
+    Thread.Sleep(1000)
+    Dim strHref As String = ""
+
+
+    If WebBrowser1.Url.ToString.ToLower.StartsWith(strActivityURL.ToLower) Then
+      Dim colTags As HtmlElementCollection = WebBrowser1.Document.GetElementsByTagName("a")
+      For Each uTag As HtmlElement In colTags
+        strHref = uTag.GetAttribute("href")
+        If strHref.ToLower.Contains("/allactivity/removecontent/") Or strHref.ToLower.Contains("/allactivity/delete/") Then
+          If strDeleteURLs.Contains(strHref) = False Then
+            Log("-Delete Link: " & strHref)
+            strDeleteURLs = strDeleteURLs & strHref & vbTab
+          End If
+        End If
+      Next
+    End If
+
+  End Sub
+
+  Sub CollectURLs()
+    Thread.Sleep(1000)
+    Dim strHref As String = ""
+    If WebBrowser1.Url.ToString.ToLower.StartsWith(strActivityURL.ToLower) Then
+      Dim colTags As HtmlElementCollection = WebBrowser1.Document.GetElementsByTagName("a")
+      For Each uTag As HtmlElement In colTags
+        strHref = uTag.GetAttribute("href")
+        If strHref.ToLower.Contains("/allactivity?timeend=") Then
+          Dim strLinkText As String = ""
+          Try
+            strLinkText = uTag.InnerText
+          Catch ex As Exception
+            strLinkText = ""
+          End Try
+          'And strLinkText.ToLower.Contains("load more") = False
+          If strLinkText Is Nothing Then strLinkText = ""
+          If strURLList.Contains(strHref) = False And strLinkText <> "" And strLinkText.ToLower <> "back to top" Then
+            Log("# " & strLinkText.Replace(vbCr, "").Replace(vbLf, "") & "-> " & strHref)
+            strURLList = strURLList & strHref & vbTab
+            blnFoundLinks = True
+            blnFoundDeeper = True
+          End If
+        End If
+      Next
+    End If
+
+
+  End Sub
+  Sub LookForLM(strURL As String)
+    Thread.Sleep(1000)
+    BrowseTo(strURL)
+    Dim strHref As String = ""
+
+    If WebBrowser1.Url.ToString.ToLower.StartsWith(strActivityURL.ToLower) Then
+      Dim colTags As HtmlElementCollection = WebBrowser1.Document.GetElementsByTagName("a")
+      For Each uTag As HtmlElement In colTags
+        Try
+          strHref = uTag.GetAttribute("href")
+          If strHref.ToLower.Contains("/allactivity?timeend=") Then
+            Dim strLinkText As String = ""
+            Try
+              strLinkText = uTag.InnerText
+            Catch ex As Exception
+              strLinkText = ""
+            End Try
+            'And strLinkText.ToLower.Contains("load more") = False
+            If strLinkText Is Nothing Then strLinkText = ""
+            If strLinkText.ToLower.Contains("load more") = True And strURLList.Contains(strHref) = False Then
+              Log("# " & strLinkText.Replace(vbCr, "").Replace(vbLf, "") & " (" & intLoadMoreLevels & ")-> " & strHref)
+              strURLList = strURLList & strHref & vbTab
+              intLoadMoreLevels = intLoadMoreLevels + 1
+              BrowseTo("about:blank")
+              LookForLM(strHref)
+              blnFoundLinks = True
+              blnFoundDeeper = True
+            End If
+          End If
+        Catch ex As Exception
+          'Log("# Bad <a> tag hit!")
+        End Try
+
+      Next
+    End If
+
+  End Sub
+
+
+  Sub Log(strText As String)
+    TextBox1.Text = TextBox1.Text & strText & vbCrLf
+    TextBox1.SelectionStart = TextBox1.Text.Length
+    TextBox1.ScrollToCaret()
+    TextBox1.Refresh()
+  End Sub
+
+  Private Sub WebBrowser1_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs) Handles WebBrowser1.DocumentCompleted
+    Me.TextBox2.Text = "Current Page: " & WebBrowser1.Url.ToString
+    Me.Text = txtAppTitle & " - " & Me.WebBrowser1.DocumentTitle
+    blnIsBrowsing = False
+
+    If Me.WebBrowser1.Url.ToString.ToLower.Contains("/login/device-based/password/") = True Then
+      'NavToActivityPage()
+    End If
+
+    If Me.WebBrowser1.Url.ToString.ToLower.Contains("/allactivity") = True And strFacebookUserID = "" Then
+      Dim arrParts() As String = Me.WebBrowser1.Url.ToString.Split("/")
+      strFacebookUserID = arrParts(3)
+      Log("Facebook User ID: " & strFacebookUserID)
+      strActivityURL = "https://mbasic.facebook.com/" & strFacebookUserID.Trim & "/allactivity"
+    End If
+
+  End Sub
+
+  Sub NavToActivityPage()
+    Dim colTags As HtmlElementCollection = WebBrowser1.Document.GetElementsByTagName("a")
+    Dim strHref As String = ""
+    For Each uTag As HtmlElement In colTags
+      Try
+        If uTag.InnerText.ToLower = "profile" Then
+          Dim uHref As String = uTag.GetAttribute("href")
+          BrowseTo(uHref)
+          Dim colTagsB As HtmlElementCollection = WebBrowser1.Document.GetElementsByTagName("a")
+          For Each zTag As HtmlElement In colTagsB
+            Try
+              uHref = zTag.GetAttribute("href")
+              If uHref.ToLower.Contains("/allactivity") = True Then
+                BrowseTo(uHref)
+
+                Exit For
+              End If
+            Catch ex As Exception
+            End Try
+          Next
+          Exit For
+        End If
+      Catch ex As Exception
+
+      End Try
+    Next
+  End Sub
+  Private Sub Form1_Closed(sender As Object, e As EventArgs) Handles Me.Closed
+    End
+  End Sub
+  Private Sub btnStop_Click(sender As Object, e As EventArgs) Handles btnStop.Click
+    blnStop = True
+    MsgBox("Aborted!", MsgBoxStyle.Exclamation, Me.Text)
+  End Sub
+
+  Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    LetsGo("cluster_116")
+  End Sub
+
+  Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+    LetsGo("likes")
+  End Sub
+  Private Sub Button3_Click_1(sender As Object, e As EventArgs) Handles Button3.Click
+    LetsGo("cluster_11")
+  End Sub
+
+  Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+    LetsGo("friends")
+  End Sub
+
+  Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+    LetsGo("cluster_202")
+  End Sub
+
+  Private Sub Button6_Click(sender As Object, e As EventArgs)
+    LetsGo("search")
+  End Sub
+
+  Private Sub Button6_Click_1(sender As Object, e As EventArgs) Handles Button6.Click
+    Dim strFilterName As String = InputBox("Enter the 'log_filter' value for the Activity you want to delete." & vbCrLf & vbCrLf & "(Depending on the activity, this may not work!)", "ADVANCED OPTION", "")
+    If strFilterName <> "" Then
+      LetsGo(strFilterName)
+    End If
+  End Sub
+  Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+    e.Handled = True
+    e.KeyChar = Nothing
+  End Sub
+End Class
